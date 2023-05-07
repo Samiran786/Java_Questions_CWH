8033647722fae8bff68196bfa65f4b8c0c0e76e2
Samiran Banerjee <116157715+Samiran786@users.noreply.github.com>
2023-05-07 17:12:46 +0530

    Add files via upload

---
+++ cwh_lec19_Conditional_Problem_04.java
@@ -0,0 +1,32 @@
+/*
+ * Q4) Write a Java program to find whether a year entered by the user is a leap year or not.`
+ *     
+ * 
+ * 
+ * @ Author : Samiran Banerjee
+ * @ Date : 05-05-2023
+ * 
+ * 
+ * 
+ * */
+
+
+package Java_Questions;
+
+import java.util.Scanner;
+
+public class cwh_lec19_Conditional_Problem_04 {
+	
+	public static void main(String[] args) {
+	     Scanner sc = new Scanner(System.in);
+	     System.out.println("Enter the year");
+	     int yr = sc.nextInt();
+	     
+	     if(yr%4==0) {
+	    	 System.out.println("The given year "+yr+" is a leap year");
+	     }else {
+	    	 System.out.println("The given year "+yr+" is not a leap year");
+	     }
+	}
+
+}
